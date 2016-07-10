0746bbcdec20ae9dfe0c9d1868c8b18c16476ee4
Fernando-Lima <fernandosupweb@gmail.com>
2016-07-10 15:48:22 -0300

    Implementado Intent para navegação entre telas

--- app/src/main/java/com/example/fernando/helpme/MainActivity.java
+++ app/src/main/java/com/example/fernando/helpme/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.fernando.helpme;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends DebugActivity {
+public class MainActivity extends DebugActivity implements View.OnClickListener {
 
-    Button btnNovo, btnAlert;
+    Button btnContato, btnAlerta;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnContato = (Button) findViewById(R.id.btnContato);
+        btnContato.setOnClickListener(this);
+    }
 
-
-
-
+    @Override
+    public void onClick(View v) {
+        Intent it = new Intent(this, ContatoActivity.class);
+        startActivity(it);
+    }
 }
