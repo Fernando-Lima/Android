7263168c426af0d00a9880a783593f0e4af4612c
Fernando-Lima <fernandosupweb@gmail.com>
2016-07-10 15:55:57 -0300

    Implementado tela de contato

---
+++ app/src/main/java/com/example/fernando/helpme/ContatoActivity.java
@@ -0,0 +1,13 @@
+package com.example.fernando.helpme;
+
+
+import android.os.Bundle;
+
+public class ContatoActivity extends DebugActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contato);
+    }
+}
