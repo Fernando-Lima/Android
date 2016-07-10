421a52fe2f3e5613791c1dd492fe745225784ee5
Fernando-Lima <fernandosupweb@gmail.com>
2016-07-10 02:25:20 -0300

    Initial commit

---
+++ app/src/main/java/com/example/fernando/helpme/DebugActivity.java
@@ -0,0 +1,60 @@
+package com.example.fernando.helpme;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+/**
+ * Created by fernando on 06/07/16.
+ */
+public class DebugActivity extends AppCompatActivity{
+    protected static final String TAG = "ciclo";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(TAG,getLocalClassName()+ ".onSaveInstaceState");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(TAG, getLocalClassName()+ ".onStart()");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG,getLocalClassName()+ ".onRestar()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG,getLocalClassName()+ ".onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG,getLocalClassName()+ ".onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, getLocalClassName()+ ".onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG,getLocalClassName()+ ".onDestroy()");
+    }
+}
---
+++ app/src/main/java/com/example/fernando/helpme/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.fernando.helpme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends DebugActivity {
+
+    Button btnNovo, btnAlert;
+
+
+
+
+}
